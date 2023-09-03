47f80e9298f5f51e6cdf05c9cddcd987629db5f7
Adriana133 <adrianasilva487@yahoo.com>
2023-09-03 17:51:57 -0300

    first-commit

---
+++ Desafio CCXP/main.js
@@ -0,0 +1,74 @@
+const ingressos = [];
+
+function addRedBorder(id) {
+    element = document.querySelector( `#${id})`);
+    element.style.border = "5px solid red";
+}
+
+function highLightCard(selector) {
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-highLight");
+}
+
+function checkkeyboardcode() {
+//add event listener on keydown
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    //alert the key name and code on keydown
+    alert(`Tecla pressionada ${name} \r\n key code: ${code}`);
+}, false);
+}
+
+function addkeyboardEventListener() {
+    document.addEventListener('keydown', (event) => {
+
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = event.code;
+        if(code == 'Digit1'){
+            ingresso1.classList.toggle("card-highLight");
+            ingresso2.classList.remove("card-highLight");
+            ingresso3.classList.remove("card-highLight");
+            ingresso4.classList.remove("card-highLight");
+        }
+
+        if(code == 'Digit2'){
+            ingresso1.classList.remove("card-highLight");
+            ingresso2.classList.toggle("card-highLight");
+            ingresso3.classList.remove("card-highLight");
+            ingresso4.classList.remove("card-highLight");
+        }
+
+        if(code == 'Digit3'){
+            ingresso1.classList.remove("card-highLight");
+            ingresso2.classList.remove("card-highLight");
+            ingresso3.classList.toggle("card-highLight");
+            ingresso4.classList.remove("card-highLight");
+        }
+
+        if(code == 'Digit4'){
+            ingresso1.classList.remove("card-highLight");
+            ingresso2.classList.remove("card-highLight");
+            ingresso3.classList.remove("card-highLight");
+            ingresso4.classList.toggle("card-highLight");
+        }
+
+    }, false);
+}
+
+selectCard = (selector) => {
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+    if(ingressos.includes(selector)) ingressos.pop(selector);
+    else ingressos.push(selector)
+}
+
+showSelectedCards = () => {
+    if(ingressos.length > 0) alert("Ingressos Selecionados:" + ingressos);
+}
+
+addkeyboardEventListener();
